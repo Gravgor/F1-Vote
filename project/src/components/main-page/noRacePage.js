82eef4f9fef672d9e25bfa0503e88b58b24ed5a0
Gravgor <36999577+Gravgor@users.noreply.github.com>
2022-10-06 01:34:10 +0200

    Update noRacePage.js

--- project/src/components/main-page/noRacePage.js
+++ project/src/components/main-page/noRacePage.js
@@ -20,7 +20,7 @@ export class NoRace extends React.Component{
         return(
             <div>
                 <div className="header" style={{position: 'relative', top: '250px'}}>
-                <h1 className="page-title" style={{fontFamily: 'F1-Regular'}}>F1 Vote for Winner -</h1>
+                <h1 className="page-title" style={{fontFamily: 'F1-Regular'}}>F1 Vote -</h1>
                 <p className="actual-grandprix" style={{fontFamily: 'F1-Regular'}}>Next Grand Prix in next Week!</p>
                 <button className="sign-in-admin" style={{fontFamily: 'F1-Button'}}><BiUser style={{position: 'relative', top: '2px'}} onClick={this.navigateAdmin}/> Sign In</button>
                </div>
